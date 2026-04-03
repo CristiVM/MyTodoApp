9171ca4f72f8be40a5ea166c9e55f79315030231
CristiVM <cristianom.2004@gmail.com>
2026-04-03 20:59:37 +0100

    feat: authentication on backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import CreateUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path("api/user/register/", CreateUserView.as_view(), name='register'),
+    path("api-auth/", include("rest_framework.urls")),
 ]
